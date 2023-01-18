5c72c9130556d8acfce45a99908a80d91c1e02fe
SkPhilipp <philipp@hileco.com>
2023-01-18 17:58:40 +0100

    Updated lint.py,rules.py

--- lint.py
+++ lint.py
@@ -2,24 +2,59 @@
 
 import sys
 import ruamel.yaml
+import rules
 
 
-def lint_document(document_source_path, document_source_index, document):
-    print("# document: {}[{}]".format(document_source_path, document_source_index))
-    pass
+def log_info(document_source, document_source_index, message):
+    print("info for {}[{}]: {}".format(document_source, document_source_index, message))
 
 
-def lint_file(yaml_file_path):
-    yaml_stream = sys.stdin if yaml_file_path == '-' else open(yaml_file_path)
+def log_error(document_source, document_source_index, message):
+    print("error for {}[{}]: {}".format(document_source, document_source_index, message))
+
+
+def log_warning(document_source, document_source_index, message):
+    print("warning for {}[{}]: {}".format(document_source, document_source_index, message))
+
+
+def lint_document(document_source, document_source_index, document):
+    if "apiVersion" not in document:
+        log_info(document_source, document_source_index, "document skipped, missing apiVersion")
+        return
+    if document["apiVersion"] != "argoproj.io/v1alpha1":
+        log_info(document_source, document_source_index, "document skipped, apiVersion is not argoproj.io/v1alpha1")
+        return
+    if "kind" not in document:
+        log_info(document_source, document_source_index, "document skipped, missing kind")
+        return
+    if document["kind"] != "Application":
+        log_info(document_source, document_source_index, "document skipped, kind is not Application")
+        return
+    queue = []
+    queue.extend(rules.top)
+    rules_applied = 0
+    while len(queue) > 0:
+        rule = queue.pop()
+        try:
+            rules_applied += 1
+            continuations = rule(document)
+            queue = continuations + queue
+        except Exception as e:
+            log_error(document_source, document_source_index, "rule {} failed: {}".format(rule.__name__, e))
+    log_info(document_source, document_source_index, "{} linter rules applied".format(rules_applied))
+
+
+def lint_file(yaml_source):
+    yaml_stream = sys.stdin if yaml_source == '-' else open(yaml_source)
     with yaml_stream:
         documents = ruamel.yaml.load_all(yaml_stream, Loader=ruamel.yaml.RoundTripLoader)
         for document_index, document in enumerate(documents):
-            lint_document(yaml_file_path, document_index, document)
+            lint_document(yaml_source, document_index, document)
 
 
 def main():
-    for yaml_file_path in sys.argv[1:]:
-        lint_file(yaml_file_path)
+    for yaml_source in sys.argv[1:]:
+        lint_file(yaml_source)
 
 
 if __name__ == '__main__':
---
+++ rules.py
@@ -0,0 +1,91 @@
+def rule_metadata_finalizer(document):
+    if "metadata" not in document:
+        raise Exception("missing .metadata")
+    if "finalizers" not in document["metadata"]:
+        raise Exception("missing .metadata.finalizers")
+    if "resources-finalizer.argocd.argoproj.io" not in document["metadata"]["finalizers"]:
+        raise Exception("missing .metadata.finalizers.resources-finalizer.argocd.argoproj.io")
+    return []
+
+
+def rule_destination_namespace(document):
+    if "spec" not in document:
+        raise Exception("missing .spec")
+    if "destination" not in document["spec"]:
+        raise Exception("missing .spec.destination")
+    if "namespace" not in document["spec"]["destination"]:
+        raise Exception("missing .spec.destination.namespace")
+    if "syncPolicy" in document["spec"]:
+        if "syncOptions" in document["spec"]["syncPolicy"]:
+            if "CreateNamespace=true" in document["spec"]["syncPolicy"]["syncOptions"]:
+                return
+    # verify namespace exists
+    return []
+
+
+def rule_destination_server(document):
+    if "spec" not in document:
+        raise Exception("missing .spec")
+    if "destination" not in document["spec"]:
+        raise Exception("missing .spec.destination")
+    if "server" not in document["spec"]["destination"]:
+        raise Exception("missing .spec.destination.server")
+    # verify server exists
+    return []
+
+
+def rule_project_exists(document):
+    if "spec" not in document:
+        raise Exception("missing .spec")
+    if "project" not in document["spec"]:
+        raise Exception("missing .spec.project")
+    # verify project exists
+    return [rule_project_exists_and_namespace_allowed, rule_project_exists_and_server_allowed]
+
+
+def rule_project_exists_and_namespace_allowed(document):
+    project = document["spec"]["project"]
+    # verify project allows for deployment into .spec.destination.namespace
+    return []
+
+
+def rule_project_exists_and_server_allowed(document):
+    project = document["spec"]["project"]
+    # verify project allows for deployment into .spec.destination.server
+    return []
+
+
+def rule_source_repo_accessible(document):
+    if "spec" not in document:
+        raise Exception("missing .spec")
+    if "source" not in document["spec"]:
+        raise Exception("missing .spec.source")
+    if "repoURL" not in document["spec"]["source"]:
+        raise Exception("missing .spec.source.repoURL")
+    # verify repoURL accessible
+    return [rule_source_repo_accessible_revision]
+
+
+def rule_source_repo_accessible_revision(document):
+    source = document["spec"]["source"]
+    if "targetRevision" not in source:
+        raise Exception("missing .spec.source.targetRevision")
+    # verify targetRevision exists in repository
+    return [rule_source_repo_accessible_revision_and_path]
+
+
+def rule_source_repo_accessible_revision_and_path(document):
+    source = document["spec"]["source"]
+    if "path" not in source:
+        raise Exception("missing .spec.source.path")
+    # verify path exists in repository at targetRevision
+    return []
+
+
+top = [
+    rule_metadata_finalizer,
+    rule_destination_namespace,
+    rule_destination_server,
+    rule_project_exists,
+    rule_source_repo_accessible,
+]
